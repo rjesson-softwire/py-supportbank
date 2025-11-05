a4ee00cce2ad312cae601d7af9d7fddd92a638be
Rebecca Jesson <rebecca.jesson@softwire.com>
2025-11-05 15:40:45 +0000

    Part 2

--- supportbank.py
+++ supportbank.py
@@ -1,13 +1,21 @@
 import csv
+import logging
+import re
 
 def all_accounts(file):
     balances = {}
     for row in file:
         debtor = row[2]
         creditor = row[1]
-        amount = float(row[4])
-        balances[debtor] = balances.get(debtor, 0) - amount
-        balances[creditor] = balances.get(creditor, 0) + amount
+        try:
+            amount = float(row[4])
+            balances[debtor] = balances.get(debtor, 0) - amount
+            balances[creditor] = balances.get(creditor, 0) + amount
+        except ValueError:
+            logger.error("Invalid amount")
+            print("ERROR: Invalid amount in:")
+            print(row)
+            print("Emitting this entry.")
 
     for person in balances:
         print(person, ": £", round(balances[person], 2))
@@ -19,8 +27,13 @@ def account(file, account):
         if row[2] == account:
             print(row[0], ": Borrowed £", row[4], " from ", row[1], " for ", row[3])
 
+logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.info("Started")
+
 with open("DodgyTransactions2015.csv", mode='r') as csvfile:
     f = csv.reader(csvfile)
+    logger.info("Reading file")
 
     next(f)
 
@@ -31,3 +44,5 @@ with open("DodgyTransactions2015.csv", mode='r') as csvfile:
         all_accounts(f)
     else:
         account(f, option)
+
+logger.info("Finished")
