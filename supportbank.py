6aba48a3efd83aa37c78dd5e27a35f72b5ec83b1
Rebecca Jesson <rebecca.jesson@softwire.com>
2025-11-06 11:13:45 +0000

    Part 4: Add support for XML files

--- supportbank.py
+++ supportbank.py
@@ -1,6 +1,8 @@
 import csv
 import json
+from bs4 import BeautifulSoup
 import logging
+import datetime
 
 logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -20,11 +22,13 @@ class Account:
         self.balance = 0
 
     def display_own_transactions(self, transactions):
+        output = ""
         for t in transactions:
             if t.debtor == self.name:
-                print(t.date, ": Borrowed £", t.amount, " from ", t.creditor, " for ", t.narrative)
+                output += str(t.date) + ": Borrowed £" + str(t.amount) + " from " + t.creditor + " for " + t.narrative + "\n"
             elif t.creditor == self.name:
-                print(t.date, ": Lent £", t.amount, " to ", t.debtor, " for ", t.narrative)
+                output += str(t.date) + ": Lent £" + str(t.amount) + " to " + t.debtor + " for " + t.narrative + "\n"
+        return output
 
     def calculate_balance(self, transactions):
         for t in transactions:
@@ -33,23 +37,26 @@ class Account:
             elif t.creditor == self.name:
                 self.balance += t.amount
 
-
-filetype = input("Enter file type: ")
-
 def banking(data):
     transactions, accounts = process_data(data)
 
     print("Please enter List All to see all balances, or List [Account] to see the transactions of an individual.")
     option = input("Enter your option: ")[5:]
 
+    output = ""
+
     if option == "All":
         for a in accounts:
             a.calculate_balance(transactions)
-            print(a.name, ": £", round(a.balance, 2))
+            text = a.name + ": £" + str(round(a.balance, 2))
+            output += text + "\n"
     else:
         for a in accounts:
             if option == a.name:
-                a.display_own_transactions(transactions)
+                output += a.display_own_transactions(transactions)
+
+    print(output)
+
 
 def process_data(data):
     transactions = []
@@ -70,6 +77,12 @@ def process_data(data):
         accounts.append(Account(n))
     return transactions, accounts
 
+def excel_date_to_date(serial_date):
+    base_date = datetime.date(1899,12,30)
+    return base_date + datetime.timedelta(days = int(serial_date))
+
+filetype = input("Enter file type: ")
+
 if filetype == "csv":
     with open("DodgyTransactions2015.csv", mode='r') as csvfile:
         f = csv.reader(csvfile)
@@ -78,16 +91,44 @@ if filetype == "csv":
         next(f)
 
         banking(f)
-
 elif filetype == "json":
     with open ("Transactions2013.json", 'r') as jsonfile:
         f = json.load(jsonfile)
         logger.info("Reading json file")
 
-        data = [
+        data = []
 
         for row in f:
-            data.append([row['Date'], row['FromAccount'], row['ToAccount'], row['Narrative'], row['Amount']])
+            data.append([row['Date'][:10], row['FromAccount'], row['ToAccount'], row['Narrative'], row['Amount']])
+
+        banking(data)
+elif filetype == "xml":
+    with open("Transactions2012.xml", 'r') as xmlfile:
+        f = xmlfile.read()
+        logger.info("Reading xml file")
+
+        bs_data = BeautifulSoup(f, "lxml")
+
+        bs_transactions = bs_data.find_all('supporttransaction')
+
+        data = []
+
+        for t in bs_transactions:
+            serial_date = t.get('date')
+            date = excel_date_to_date(serial_date)
+            narrative = t.find('description').text
+            parties = t.find('parties')
+            to_account = parties.find('to').text
+            from_account = parties.find('from').text
+            try:
+                amount = t.find('value').text
+            except AttributeError:
+                logger.error("No value on transaction")
+                print("ERROR: No value on transaction:")
+                amount = 0
+                print(date, ":", "from", from_account, "to", to_account, "for", narrative)
+
+            data.append([date, from_account, to_account, narrative, amount])
 
         banking(data)
 
